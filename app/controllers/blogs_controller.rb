d9b4d0cb97d1c75901b615443752c63f18045e7b
tetsu05 <tetsu051955@gmail.com>
2021-09-11 12:18:30 +0000

    [Finish]3-4

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all
   end
 
   def show
@@ -7,9 +8,9 @@ class BlogsController < ApplicationController
 
   def new
     @blog = Blog.new
-    
+
   end
-  
+
   def create
     blog = Blog.new(blog_params)
     blog.save
@@ -18,10 +19,10 @@ class BlogsController < ApplicationController
 
   def edit
   end
-  
+
    private
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
   end
-  
+
 end
